fc4ff0993fbe28be629e251d641566e27cea6751
thiagoKenzie <102174562+thiagoKenzie@users.noreply.github.com>
2023-04-10 12:14:58 -0300

    Add files via upload

---
+++ Sprint 3/1_introducao_jaavscript/script.js
@@ -0,0 +1,57 @@
+console.log('Thiago');
+console.log('Kenzie Academy');
+console.log('Demo 10/04');
+
+//alert('Thiago')
+
+console.log('Executou o alert');
+
+
+const nome = 'Thiago'; 
+let idade = 25; 
+{
+    let idade = 12;
+    console.log(idade);
+}
+
+console.log(nome)
+console.log(idade)
+
+idade = 26;
+
+console.log(nome)
+console.log(idade)
+
+//STRING
+let texto = 'Teste' 
+let texto2 = "Teste"
+let texto3 = `Teste`
+console.log(typeof texto)
+
+//NUMBER
+let numero = 1231298037198273
+let numero2 = -123129803719827
+let numero3 = 3.1415170
+console.log(typeof numero)
+
+//BOOLEAN
+let boleano1 = true
+let boleano2 = false
+console.log(typeof boleano1)
+
+boleano1 = 'Thiago'
+
+console.log(typeof boleano1)
+
+
+let teste = "Thiago";
+let soma = 0;
+
+teste = 'Maria';
+soma = soma + 1;
+
+teste = 'Tereza';
+soma = soma + 1;
+
+console.log(soma);
+
---
+++ Sprint 3/1_introducao_jaavscript/script2.js
@@ -0,0 +1,9 @@
+//EXERCICIO 1
+/*
+let decimal = 10.13;
+let inteiro = 15;
+let string = 'Teste';
+console.log(decimal, inteiro, string);
+*/
+
+//EXERCICIO 2
